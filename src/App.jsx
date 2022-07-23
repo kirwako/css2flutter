ec65049948f1c86a47ded70e213ed01bce65b443
Imran Baali <ibaali@e1r12p4.1337.ma>
2022-07-23 18:38:19 +0100

    add tabout google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Logo from "./components/TopSection/TopSection";
 import OperatingArea from "./components/OperatingArea/OperatingArea";
 import Footer from "./components/Footer/Footer";
-import ReactGA from "react-ga";
-const TRACKING_ID = "UA-148002086-1"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <>
       <Logo />
